8fd0e6aff478640e61e6efe4c11cdac002c19211
AllsGamingHD <allgaminghd1@gmail.com>
2018-08-22 04:32:11 +0200

    Update BETAFORM.cs
    Lien de l'API enlever pour éviter des problèmes avec des malins qui pourrait s'amuser avec ^^

--- Optimisation Windows Capet/Base/UserControl/BETAFORM.cs
+++ Optimisation Windows Capet/Base/UserControl/BETAFORM.cs
@@ -23,7 +23,7 @@ namespace Optimisation_Windows_Capet.Base.UserControl
     {
         UdpClient server = new UdpClient();
         WebClient client = new WebClient();
-        string URL = "http://lossantoscityfr.esy.es/";
+        string URL = "APIURL Private";
         public BETAFORM()
         {
             InitializeComponent();
@@ -108,114 +108,6 @@ namespace Optimisation_Windows_Capet.Base.UserControl
         private async void simpleButton1_Click(object sender, EventArgs e)
         {
             progressPanel2.Visible = true;
-            if (checkEdit1.Checked == true)
-            {
-                Properties.Settings.Default.CheckBeta = checkEdit1.CheckState.ToString();
-                Properties.Settings.Default.PassBeta = textEdit1.Text;
-                Properties.Settings.Default.UserBeta = textEdit2.Text;
-                Properties.Settings.Default.Save();
-            }
-            else
-            {
-                Properties.Settings.Default.CheckBeta = "Unchecked";
-                Properties.Settings.Default.Save();
-
-            }
-            await Task.Delay(500);
-            try
-            {
-                string thisguid = getUniqueID("C");
-                server.Connect("localhost", 80);
-                string loginusername = textEdit1.Text;
-                string loginpassword = textEdit2.Text;
-                if (client.DownloadString(URL + "api.php?type=login&username=" + textEdit1.Text + "&password=" + textEdit2.Text + "&hwid=" + thisguid).Contains("0x05"))
-                {
-                   progressPanel2.Visible = false;
-
-                   XtraMessageBox.Show("Vous êtes connectée !\nBonne continuation", "Succès!", MessageBoxButtons.OK, MessageBoxIcon.Information); ;
-                    if (Properties.Settings.Default.LaunchAccueil == "PasOuvert")
-                    {
-                        Accueil AC = new Accueil();
-                        this.Hide();
-                        AC.Show();
-                    }
-                    else
-                    {
-                        Form1 F1 = new Form1();
-                        this.Hide();
-                        F1.Show();
-                    }
-                    //Open form, or do whatever people do here.
-                    
-                }
-                else if (client.DownloadString(URL + "api.php?type=login&username=" + textEdit1.Text + "&password=" + textEdit2.Text + "&hwid=" + thisguid).Contains("0x01"))
-                {
-                   progressPanel2.Visible = false;
-
-
-                    XtraMessageBox.Show("HWID ne correspond pas :(", "Erreur");
-                }
-                else if (client.DownloadString(URL + "api.php?type=login&username=" + textEdit1.Text + "&password=" + textEdit2.Text + "&hwid=" + thisguid).Contains("0x02"))
-                {
-                   progressPanel2.Visible = false;
-
-                   XtraMessageBox.Show("Veuillez remplir toutes les cases.", "Erreur");
-                }
-                else if (client.DownloadString(URL + "api.php?type=login&username=" + textEdit1.Text + "&password=" + textEdit2.Text + "&hwid=" + thisguid).Contains("0x03"))
-                {
-                   progressPanel2.Visible = false;
-
-                   XtraMessageBox.Show("Nom d'utilisateur ou mot de passe incorrect.", "Erreur");
-                }
-                else if (client.DownloadString(URL + "api.php?type=login&username=" + textEdit1.Text + "&password=" + textEdit2.Text + "&hwid=" + thisguid).Contains("0x04"))
-                {
-                   progressPanel2.Visible = false;
-
-                   XtraMessageBox.Show("Vous êtes banni.", "Erreur");
-                }
-            }
-            catch (WebSocketException)
-            {
-               progressPanel2.Visible = false;
-               XtraMessageBox.Show("Serveur pas disponible", "Erreur Serveur", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
-            }
-        }
-
-        private void checkEdit1_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void checkEdit2_CheckedChanged(object sender, EventArgs e)
-        {
-            if (checkEdit2.Checked == true)
-            {
-                textEdit2.Properties.UseSystemPasswordChar = false;
-            }
-            else
-            {
-                textEdit2.Properties.UseSystemPasswordChar = true;
-
-            }
-        }
-
-        private void checkEdit3_CheckedChanged(object sender, EventArgs e)
-        {
-            if (checkEdit3.Checked == true)
-            {
-                textEdit3.Properties.UseSystemPasswordChar = false;
-            }
-            else
-            {
-                textEdit3.Properties.UseSystemPasswordChar = true;
-            }
-        }
-
-        private async void BETAFORM_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            if (e.KeyChar == 13)
-              {
-                  progressPanel2.Visible = true;
                   if (checkEdit1.Checked == true)
                   {
                       Properties.Settings.Default.CheckBeta = checkEdit1.CheckState.ToString();
@@ -287,7 +179,41 @@ namespace Optimisation_Windows_Capet.Base.UserControl
                       progressPanel2.Visible = false;
                       XtraMessageBox.Show("Serveur pas disponible", "Erreur Serveur", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
                   }
-              }
+        }
+
+        private void checkEdit1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkEdit2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkEdit2.Checked == true)
+            {
+                textEdit2.Properties.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                textEdit2.Properties.UseSystemPasswordChar = true;
+
+            }
+        }
+
+        private void checkEdit3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkEdit3.Checked == true)
+            {
+                textEdit3.Properties.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                textEdit3.Properties.UseSystemPasswordChar = true;
+            }
+        }
+
+        private async void BETAFORM_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            simpleButton1.PerformClick();
         }
     }
 }
